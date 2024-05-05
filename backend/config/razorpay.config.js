115e1e5e9929a0dd48ceb446835c850ed834acae
NirajD10 <niraj.deshmukh1999@gmail.com>
2024-05-05 17:44:04 +0530

    cleanup

--- backend/config/razorpay.config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const Razorpay = require("razorpay") ;
-
-const razorpayinstance = new Razorpay({
-	key_id: process.env.RAZORPAY_API_KEY,
-	key_secret: process.env.RAZORPAY_SECRET_KEY,
-});
-
-module.exports = razorpayinstance
